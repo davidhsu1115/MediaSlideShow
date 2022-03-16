12395d592cb48caf738d9815339abdb69f1bd18c
david.hsu <david.hsu@mtaxi.com.tw>
2022-03-16 13:24:33 +0800

    Initial Commit

---
+++ CustomSlideShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSlideShow
+//
+//  Created by Mtaxi on 2022/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
